2994f7dfaefebe03813978c82f7f5f19ad88a870
lc <443468449@qq.com>
2019-09-18 12:34:00 +0800

    mybatis 多数据源

--- final-project/src/main/java/com/lc/controller/HelloController.java
+++ final-project/src/main/java/com/lc/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.lc.controller;
 
+import com.lc.mapper.test1.User1Mapper;
+import com.lc.mapper.test2.User2Mapper;
+import com.lc.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +14,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    User1Mapper userMapper;
+
+    @Autowired
+    User2Mapper userMapper2;
+
 
     @GetMapping
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("addUser1")
+    public String addUser1(){
+        User user = new User();
+        user.setName("db1User1");
+        user.setPassword("db1User1");
+        userMapper.insert(user);
+        return "addUser1 Success";
+    }
+
+    @GetMapping("addUser2")
+    public String addUser2(){
+        User user = new User();
+        user.setName("db1User2");
+        user.setPassword("db1User2");
+        userMapper2.insert(user);
+        return "addUser2 Success";
     }
 }

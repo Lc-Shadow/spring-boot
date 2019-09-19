6e3cf812cbb156bb151b8ceb2cc284476fb054b1
lc <443468449@qq.com>
2019-09-19 09:01:52 +0800

    提交finalObject

---
+++ final-project/src/main/java/com/lc/mapper/test1/User1Mapper.java
@@ -0,0 +1,7 @@
+package com.lc.mapper.test1;
+
+import com.lc.model.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface User1Mapper extends Mapper<User> {
+}
---
+++ final-project/src/main/java/com/lc/mapper/test2/User2Mapper.java
@@ -0,0 +1,7 @@
+package com.lc.mapper.test2;
+
+import com.lc.model.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface User2Mapper extends Mapper<User> {
+}
